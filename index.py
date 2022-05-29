16b3f87dca75392c5e03a70cea9f57ab866bc0e2
Callum <cxllm@outlook.fr>
2022-05-29 10:56:40 +0100

    cors

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
-import json
 from flask import Flask, jsonify, redirect
+from flask_cors import CORS, cross_origin
 from util.underground import underground, planned_closures
 from util.bikes import bikes
 
@@ -7,11 +7,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@cross_origin()
 def root():
     return redirect("https://github.com/cxllm/tfl-status-api#tfl-status-api")
 
 
 @app.route("/underground")
+@cross_origin()
 def underground_route():
     return jsonify(
         {"current_status": underground(), "weekend_closures": planned_closures()}
@@ -19,6 +21,7 @@ def underground_route():
 
 
 @app.route("/bikes")
+@cross_origin()
 def bike_route():
     return jsonify(bikes())
 
