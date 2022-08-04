b3e93064c4f63bf92c1a0711b9cf72e82d2daac7
Callum <cxllm@outlook.fr>
2022-08-04 21:52:27 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+	Accordion,
+	AccordionItem,
+	AccordionItemHeading,
+	AccordionItemButton,
+	AccordionItemPanel
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<
+	{},
+	{
+		tube: {
+			[key: string]: {
+				affected_stations: string[];
+				details: string | null;
+				line: string;
+				status: string;
+			};
+		};
+	}
+> {
+	interval: any;
+	constructor(props: {}) {
+		super(props);
+		this.state = {
+			tube: {}
+		};
+	}
+	getData() {
+		return fetch("/underground")
+			.then((res) => res.json())
+			.then((res) => {
+				console.log(res);
+				this.setState({ tube: res.current_status });
+			});
+	}
+	componentDidMount() {
+		this.getData();
+		this.interval = setInterval(() => {
+			this.getData();
+		}, 5 * 60 * 1000);
+	}
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+	render() {
+		return (
+			<div className="App">
+				<h2>
+					Status for the TfL network (London Undergound, Overgound, Trams and DLR)
+				</h2>
+				<hr />
+				<Accordion
+					style={{
+						backgroundColor: "#12121211",
+						padding: "0 1%",
+						textAlign: "left",
+						fontFamily: ""
+					}}
+					allowZeroExpanded
+				>
+					{Object.keys(this.state.tube).map((k, i) => {
+						let data = this.state.tube[k];
+						return (
+							<AccordionItem
+								key={i.toString()}
+								style={{
+									backgroundColor: "#35353566"
+								}}
+							>
+								<AccordionItemHeading
+									style={{
+										paddingBottom: "10px"
+									}}
+								>
+									<AccordionItemButton
+										style={{
+											backgroundColor: "inherit",
+											color: "#fff",
+											cursor: "pointer",
+											height: "40px",
+											fontSize: "medium",
+											padding: "15px"
+											/*width: 100%;
+											text-align: left;
+											border: none;*/
+										}}
+									>
+										{data.line +
+											(["DLR", "Trams", "Overground"].includes(data.line)
+												? data.line == "Trams"
+													? ""
+													: " Services"
+												: " Line")}{" "}
+										- {data.status}{" "}
+									</AccordionItemButton>
+								</AccordionItemHeading>
+								<AccordionItemPanel
+									style={{
+										fontSize: "20px"
+									}}
+								>
+									<p>{data.details || "Regular Service"}</p>
+								</AccordionItemPanel>
+							</AccordionItem>
+						);
+					})}
+				</Accordion>
+			</div>
+		);
+	}
 }
 
 export default App;
