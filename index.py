d1bf189c700e5a37d4f3288a1c333c7e5bfc4240
Callum <cxllm@outlook.fr>
2022-08-25 13:19:38 +0100

    Finishing touches on the website

--- index.py
+++ index.py
@@ -9,8 +9,7 @@ app = Flask(
     static_folder="website/build",
     static_url_path="/",
 )
-cors = CORS()
-cors.init_app(app, resources={r"*": {"origins": "*"}})
+
 app.config["CORS_HEADERS"] = "Content-Type"
 
 
@@ -25,34 +24,23 @@ def after_request(response):
 
 
 @app.route("/")
-@cross_origin(origins="*")
 def root():
-    response = redirect("https://github.com/cxllm/tfl-status-api#tfl-status-api")
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "*")
-    response.headers.add("Access-Control-Allow-Methods", "*")
     return render_template("index.html")
 
 
 @app.route("/underground")
-@cross_origin(origins="*")
 def underground_route():
     response = jsonify(
         {"current_status": underground(), "weekend_closures": planned_closures()}
     )
     response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "*")
-    response.headers.add("Access-Control-Allow-Methods", "*")
     return response
 
 
 @app.route("/bikes")
-@cross_origin(origins="*")
 def bike_route():
     response = jsonify(bikes())
     response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "*")
-    response.headers.add("Access-Control-Allow-Methods", "*")
     return response
 
 
